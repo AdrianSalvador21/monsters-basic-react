4c5ce7592cedb278f467f151e23fd8cf75e022d3
Adrian Salvador <adrian@smartlending.mx>
2020-04-15 15:50:43 -0500

    Updates

---
+++ precache-manifest.ede1a35f25f0ad41bc7e00d098666202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a7f367705df0b6eb5abeccdaf9de78",
+    "url": "/monsters-basic-react/index.html"
+  },
+  {
+    "revision": "767ecafa5b5019513959",
+    "url": "/monsters-basic-react/static/css/main.dcbd4141.chunk.css"
+  },
+  {
+    "revision": "d97e3ef4dcc9b9be121f",
+    "url": "/monsters-basic-react/static/js/2.09b1e3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-basic-react/static/js/2.09b1e3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767ecafa5b5019513959",
+    "url": "/monsters-basic-react/static/js/main.161b3e0d.chunk.js"
+  },
+  {
+    "revision": "bfa0540ecddb91739318",
+    "url": "/monsters-basic-react/static/js/runtime-main.313a7965.js"
+  }
+]);
